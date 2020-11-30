15da8a12afdfb399e1a64805cc3233aa1282610a
Iakov Salikov <36078770+isalikov@users.noreply.github.com>
2020-11-30 17:21:20 +0000

    fix: text templating preview

--- internal/view.go
+++ internal/view.go
@@ -2,18 +2,31 @@ package internal
 
 import (
 	"context"
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 )
 
+type payload struct {
+	Title string
+	Src   string
+	Text  template.HTML
+}
+
 // ViewPost response with single post
 func ViewPost(ctx context.Context, rdb *redis.Client) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		post := extractPost(ctx, c.Param("ID"), rdb)
 
-		c.HTML(http.StatusOK, "view.tmpl", post)
+		payload := payload{
+			Title: post.Title,
+			Src:   post.PreviewImage,
+			Text:  template.HTML(post.Message),
+		}
+
+		c.HTML(http.StatusOK, "view.tmpl", payload)
 
 	}
 }
